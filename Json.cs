f70cd079b2f5f6528371023d78b69ec8c6a5d017
KirillAldashkin <a89086520092k@yandex.ru>
2022-07-17 00:17:21 +0800

    File-scoped namespaces

--- Json.cs
+++ Json.cs
@@ -1,36 +1,34 @@
 ﻿using C = CircuitPlotter.ConnectionDirection;
+namespace CircuitPlotter;
 
-namespace CircuitPlotter
+internal sealed class JsonFrame
 {
-    internal sealed class JsonFrame
-    {
-        public JsonFrame(List<JsonCell> cells)
-        {
-            this.cells = cells;
-        }
+	public JsonFrame(List<JsonCell> cells)
+	{
+		this.cells = cells;
+	}
 
-        public List<JsonCell> cells { get; set; }
-    }
+	public List<JsonCell> cells { get; set; }
+}
 
-    internal sealed class JsonCell
-    {
-        public JsonCell(int x, int y, C[] connections, char symbol, string info)
-        {
-            this.x = x;
-            this.y = y;
-            this.connections = connections;
-            this.symbol = symbol;
-            this.info = info;
-        }
+internal sealed class JsonCell
+{
+	public JsonCell(int x, int y, C[] connections, char symbol, string info)
+	{
+		this.x = x;
+		this.y = y;
+		this.connections = connections;
+		this.symbol = symbol;
+		this.info = info;
+	}
 
-        public int x { get; set; }
+	public int x { get; set; }
 
-        public int y { get; set; }
+	public int y { get; set; }
 
-        public C[] connections { get; }
+	public C[] connections { get; }
 
-        public char symbol { get; set; }
+	public char symbol { get; set; }
 
-        public string info { get; set; }
-    }
+	public string info { get; set; }
 }
